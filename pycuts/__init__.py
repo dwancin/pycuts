8991ab2a5d49fee985fe6a28dafd48bd97847b01
Daniel Ialcin Misser Westergaard <dwancin@gmail.com>
2024-09-27 13:20:07 +0200

    Update __init__.py

--- pycuts/__init__.py
+++ pycuts/__init__.py
@@ -1,6 +1,6 @@
 # https://raw.githubusercontent.com/dwancin/pycuts/main/pycuts/__init__.py
 from .torch import (
-    device, gpu, empty_cache, synchronize, device_count, manual_seed
+    device, gpu, torch_dtype, empty_cache, synchronize, device_count, manual_seed
 )
 from .huggingface_hub import (
     is_spaces, is_zero_gpu_space
